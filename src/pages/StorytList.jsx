d0be98a00d4cf1fc5b6ae01a813e7d95c5f29549
afhorton <106110920+afhorton@users.noreply.github.com>
2023-12-04 17:38:18 +0000

    Here

--- src/pages/StorytList.jsx
+++ src/pages/StorytList.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import { useAuth } from '..AuthProvider';
+import { UserContext } from '..AuthProvider';
 
 function StoryList () {
-    const { currentUser } = useAuth();
+    const currentUser = useContext(UserContext);
 
     const [userPosts, setUserPosts] = useState([]);
 
