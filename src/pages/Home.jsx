e3cede5b99724badaf7c42a36397915b5e8c88aa
afhorton <106110920+afhorton@users.noreply.github.com>
2023-12-07 19:47:22 +0000

    Here

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,9 @@ function Home() {
     return (
         <div className="container vh-100 d-flex flex-column justify-content-center align-items-center">
         <h1 className="mb-4">Ulix</h1>
-        <h2 className="mb-4">Discover, Engage, Evolve: Our Blog, Your Odyssey</h2>
+        <p>/ˈjuːlɪks/</p>
+        <p><i>noun.</i> a personal chronicle, typically captivating in nature.</p>
+        <h2 className="mb-4">Discover, Engage, Evolve: Your Blog, Your Odyssey</h2>
         <div>
             <button className="btn btn-primary mr-2" onClick={goToLogin}>Login</button>
             <button className="btn btn-secondary" onClick={goToSignUp}>Signup</button>
