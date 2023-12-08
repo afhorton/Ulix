0a97e41ae43f06cdd9842d915f7dd611aa4fecb2
afhorton <106110920+afhorton@users.noreply.github.com>
2023-12-08 17:14:33 +0000

    Here

--- src/pages/EditStory.jsx
+++ src/pages/EditStory.jsx
@@ -44,12 +44,35 @@ function EditStory() {
 if (!title || !content) return null;
 
 return (
-    <div>
-        <h1><img src="public/EditStory.png" alt="Edit Story" height="100"/>Edit Story</h1>
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
-        <textarea value={content} onChange={(e) => setContent(e.target.value)}/>
-        <button onClick={handleUpdate}>Update Story</button>
-    </div>
+    // <div>
+    //     <h1><img src="public/EditStory.png" alt="Edit Story" height="100"/>Edit Story</h1>
+    //     <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+    //     <textarea value={content} onChange={(e) => setContent(e.target.value)}/>
+    //     <button onClick={handleUpdate}>Update Story</button>
+    // </div>
+
+    <div className="container vh-100 d-flex flex-column justify-content-center align-items-center">
+    <h2><img src="public/EditStory.png" alt="Edit Story" height="100"/>Edit Story</h2>
+    <form className="w-50">
+        <div className="form-group">
+            <label htmlFor="title">Title:</label>
+            <input 
+               type="text" value={title} 
+               onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+            />
+        </div>
+        <div className="form-group">
+            <label htmlFor="content">Content:</label>
+            <textarea 
+                value={content} 
+                onChange={(e) => setContent(e.target.value)} 
+                className="form-control"
+            />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3" onClick={handleUpdate}>Update Story</button>
+    </form>
+</div>
 );
 }
 
