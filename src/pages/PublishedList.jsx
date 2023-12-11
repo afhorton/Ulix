d4e7b00fea162c2320a449871622cdc6d7767176
afhorton <106110920+afhorton@users.noreply.github.com>
2023-12-11 17:20:51 +0000

    Here

---
+++ src/pages/PublishedList.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchPublishedStories } from './publishedStoriesSlice';
+
+function PublishedList () {
+const dispatch = useDispatch();
+const publishedStories = useSelector( state => state.publishedStories);
+
+useEffect(
+    () => {
+        dispatch(fetchPublishedStories());
+    }, [dispatch]
+);
+
+return (
+    <div>
+        {publishedStories.map(
+            story => (
+                <div key={story.id}>
+                    <h2>{story.title}</h2>
+                    <p>{story.content}</p>
+                    <p>Author: {story.author}</p>
+                </div>
+            ))}
+    </div>
+);
+
+};
+
+export default PublishedList;
--- src/publishedStoriesSlice.jsx
+++ src/publishedStoriesSlice.jsx
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
-import { app } from './firebase-config';
+import app from './firebase-config';
 
 const db = getFirestore(app);
 
