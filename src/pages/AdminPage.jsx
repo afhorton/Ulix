f6955414a2abdf4a16241a629d8a7017e2288a58
afhorton <106110920+afhorton@users.noreply.github.com>
2023-12-19 18:31:44 +0000

    Refactor AdminPage component to fetch and display users and published stories data

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -3,38 +3,62 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import app from "../firebase-config";
 
 function AdminPage() {
-    const [data, setData] = useState([]);
+    const [usersData, setUsersData] = useState([]);
+    const [publishedStoriesData, setPublishedStoriesData] = useState([]);
+
 
     useEffect(() => {
         const fetchData = async () => {
-            const db = firebase.firestore();
-            const data = await db.collection("users").get();
-            setData(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+            const db = getFirestore(app);
+            const usersCollection = collection(db, "users");
+            const usersSnapshot = await getDocs(usersCollection);
+            setUsersData(usersSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+        };
+        fetchData();
+    }, []);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const db = getFirestore(app);
+            const publishedStoriesCollection = collection(db, "publishedStories");
+            const publishedStoriesSnapshot = await getDocs(publishedStoriesCollection);
+            setPublishedStoriesData(publishedStoriesSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
         };
         fetchData();
     }, []);
 
+    const generateTable = (data) => (
+        <table>
+            <thead>
+                <tr>
+                    {data[0] && Object.keys(data[0]).map((key, index) => (
+                        <th key={index}>{key}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item, index) => (
+                    <tr key={index}>
+                        {Object.values(item).map((value, i) => (
+                            <td key={i}>
+                                {value instanceof Object && value.seconds ? 
+                                    new Date(value.seconds * 1000).toLocaleString() : 
+                                    value}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+
     return (
         <div>
             <h1 className="mb-4"><img src="/AdminPage.png" alt="Ulix" height="100"/>Admin Page</h1>
-            <table>
-                <thead>
-                    <tr>
-                        {data[0] && Object.keys(data[0]).map((key, index) => (
-                            <th key={index}>{key}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item, index) => (
-                        <tr key={index}>
-                            {Object.values(item).map((value, i) => (
-                                <td key={i}>{value}</td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <h2>Users</h2>
+            {generateTable(usersData)}
+            <h2>Published Stories</h2>
+            {generateTable(publishedStoriesData)}
         </div>
     );
 }
